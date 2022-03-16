e9fefaa8a476605f32922ccef34466ba6578d03a
lucaasferreira <91444111+lucaasferreira@users.noreply.github.com>
2022-03-16 18:36:25 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+let projeto1 = document.getElementById('imgproj1')
+let projeto2 = document.getElementById('imgproj2')
+let projeto3 = document.getElementById('imgproj3')
+
+function mudarImg() {
+    projeto1.addEventListener('click')
+    projeto1.innerHTML = `not found`
+}
