215077c0a17a3c97952d5e51f3213bc4c2b5dbfe
Pedro <71912007+pedro-abs@users.noreply.github.com>
2022-06-23 16:11:05 -0300

    ultimo commit, assim espero

--- test/script.js
+++
@@ -1,6 +0,0 @@
-function ToggleMenu() {
-    const nav = document.querySelector('.nav')
-    const btn = document.querySelector('.btn-mobile')
-    nav.classList.toggle('active')
-    btn.classList.toggle('active')
-}
